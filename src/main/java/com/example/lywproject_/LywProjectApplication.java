bff71c40645926f08e1da1558eb3fc62e2fb7d2e
yangchuancan <3099695295@qq.com>
2023-05-25 14:46:03 +0800

    test

---
+++ src/main/java/com/example/lywproject_/LywProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.lywproject_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+public class LywProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("1658774发哈四u回复viu啊哈867");
+    }
+
+}
---
+++ src/test/java/com/example/lywproject_/LywProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lywproject_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LywProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
